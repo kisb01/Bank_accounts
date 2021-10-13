84f9e18abba5b20cec6f1979d9a09422c4f7d911
Bíró János Zoltán <kisb01@gmail.com>
2021-10-13 19:52:45 +0200

    first commit

---
+++ src/test/java/com/tradebrite/bank_accounts/BankAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradebrite.bank_accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
