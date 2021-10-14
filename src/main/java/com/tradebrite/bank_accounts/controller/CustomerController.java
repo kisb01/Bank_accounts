eb5e0040418f939537c15c96b4100dc059b8cd65
Bíró János Zoltán <kisb01@gmail.com>
2021-10-14 11:42:27 +0200

    Implement Customer controller

--- src/main/java/com/tradebrite/bank_accounts/controller/CustomerController.java
+++ src/main/java/com/tradebrite/bank_accounts/controller/CustomerController.java
@@ -1,10 +1,10 @@
 package com.tradebrite.bank_accounts.controller;
 
+import com.tradebrite.bank_accounts.converter.StringToLong;
+import com.tradebrite.bank_accounts.model.Account;
 import com.tradebrite.bank_accounts.model.Customer;
 import com.tradebrite.bank_accounts.service.CustomerService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +21,34 @@ public class CustomerController {
     @GetMapping
     public List<Customer> findAll() {
         return customerService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Customer findById(@PathVariable("id") String id) {
+        if (StringToLong.convert(id) == null) {
+            throw new RuntimeException("Must enter a valid number");
+        } else {
+            return customerService.findById(StringToLong.convert(id));
+        }
+    }
+
+    @PostMapping
+    public Customer save(@RequestBody Customer customer) {
+        return customerService.save(customer);
+    }
+
+    @PutMapping("/{id}")
+    public Customer update(@RequestBody Customer customer, @PathVariable("id") String id) {
+        if (StringToLong.convert(id) == null) {
+            throw new RuntimeException("Must enter a valid number");
+        } else {
+            customer.setId(StringToLong.convert(id));
+        }
+        return customerService.save(customer);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable("id") String id) {
+        customerService.deleteById(StringToLong.convert(id));
     }
 }
